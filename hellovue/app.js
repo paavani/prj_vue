825199da91444c8b66601a1e4936151b59121976
paavani <pavanisd9@gmail.com>
2016-05-12 17:07:41 +0530

    write a method  to reverse the string

--- hellovue/app.js
+++ hellovue/app.js
@@ -2,5 +2,11 @@ new Vue({
 	el:"#app",
 	data: {
 		 message: 'Hello Vuejs'
+	 },
+	 methods :{
+		 reverseMessage :function(){
+		 this.message=this.message.split('').reverse().join('')
+
+		 }
 	 }
 })
